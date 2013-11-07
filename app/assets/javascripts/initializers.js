e8117b0ab481eb0c60415297910261315b187729
Kaoru <dev@kaoruk.com>
2013-11-07 09:04:02 -0600

    Adds user to view when ajax returns success, and alerts user if they are not within vicinity

--- app/assets/javascripts/initializers.js
+++ app/assets/javascripts/initializers.js
@@ -13,37 +13,15 @@
             longitude: longitude,
             latitude: latitude
           },
-          success: function(data){
-            console.log(data)
-          }
+          success: addUser
         })
       });
     }
   }
 
-  var check_if_user_is_nearby = function(currentCoords, coordinates){
-    var radius = 6371 //Earth's radius km
-      , dLon = toRad((coordinates.longitude - currentCoords.longitude))
-      , dLat = toRad((coordinates.latitude - currentCoords.latitude))
-      , lat1 = toRad(currentCoords.latitude)
-      , lat2 = toRad(coordinates.latitude)
-      , a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2)
-      , c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-      , d = radius * c * 1000
-    ;
 
-    // Check if user is within 30 meters of address.
-    if(d < 31){
-      alert("Great you're checked in!")
-      return true
-    }else{
-      alert("Sorry you're not at the event yet!")
-    }
+  var addUser = function(data){
+    var newHTML = _.getTemplate("bricks")(data)
+    $(".attendees").append(newHTML);
   }
-
-  var toRad = function(number){
-    return number * (Math.PI / 180)
-  }
-
 })();
