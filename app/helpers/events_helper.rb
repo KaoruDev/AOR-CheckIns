74ae95a1d45d1432af23286d3fccfbdc65e2761e
Kaoru <dev@kaoruk.com>
2013-11-10 20:07:57 -0600

    Removes nav _nav partial, fixes masonry delete events via AJAX

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,5 @@
 module EventsHelper
   def can_user_check_in
-    (!@attendees.include? current_user) && @event.current_event
+    (!@attendees.include? current_user) && @event.current_event && @event.date < DateTime.now.at_end_of_day
   end
 end
