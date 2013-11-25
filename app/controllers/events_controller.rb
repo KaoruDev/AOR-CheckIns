3f236d26b1f9515cb7c0dc786e26c28772293a90
Kaoru <dev@kaoruk.com>
2013-11-25 14:06:13 -0600

    Finds or creates check, to not create double check-ins

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,8 +28,7 @@ class EventsController < ApplicationController
       respond_to do |format|
         format.json{
           user = User.find(params[:user_id])
-          # check_in = @event.check_ins.build(user_id: params[:user_id])
-          # check_in.save
+          # check_in = @event.check_ins.find_or_create_by(user_id: params[:user_id])
           PrivatePub.publish_to("/messages/#{@event.id}", "init.addUser(#{user.to_json})");
           render :json => {atEvent: true}
         }
